520103a048faf683d7dab5602e88fca9b708d2bd
AlexBinkovskyy <alex_bin@ukr.net>
2024-01-14 21:42:47 +0200

    third task layout done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import { ProfileModule } from "./Profile/Profile.module";
-import userData from "../refs/userData.json"
-import { FriendList } from "./FriendList/FriendList.module";
-import friends from "../refs/friends.json"
+import { ProfileModule } from './Profile/Profile.module';
+import userData from '../refs/userData.json';
+import { FriendList } from './FriendList/FriendList.module';
+import friends from '../refs/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.module';
+import transactions from '../refs/transactions.json'
 
-
-function App (){
-  return <div>
-    <ProfileModule userData={userData}/>
-    <FriendList friends={friends}/>
-  </div>
+function App() {
+  return (
+    <div>
+      <ProfileModule userData={userData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
+    </div>
+  );
 }
 
 export default App;
