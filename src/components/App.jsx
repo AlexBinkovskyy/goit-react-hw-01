4876aae07e7c4e1b1841319e16acfde179eaa23a
AlexBinkovskyy <alex_bin@ukr.net>
2024-01-14 18:47:29 +0200

    first task layout done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
+import { ProfileModule } from "./Profile/Profile.module";
+import userData from "./userData.json"
+
 function App (){
-  return <div>App</div>
+  return <div>
+    <ProfileModule userData={userData}/>
+  </div>
 }
 
 export default App;
