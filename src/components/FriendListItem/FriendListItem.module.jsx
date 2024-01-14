70cbfa5bb6802d37450912f131aea1c3666feb3e
AlexBinkovskyy <alex_bin@ukr.net>
2024-01-15 00:02:34 +0200

    add classes

--- src/components/FriendListItem/FriendListItem.module.jsx
+++ src/components/FriendListItem/FriendListItem.module.jsx
@@ -1,10 +1,15 @@
+import "./FriendListItem.css"
+import clsx from "clsx"
+
 export function FriendListItem(data) {
   const { avatar, name, isOnline } = data.friend;
+
   return (
     <div className="item">
       <img className="avatar" src={avatar} alt={name} width="160" />
       <p className="name">{name}</p>
-      <p className="status">{isOnline ? 'online' : 'offline'}</p>
+      <p className={clsx('status', {"online": isOnline, "offline": !isOnline })}>
+        {isOnline ? 'online' : 'offline'}</p>
     </div>
   );
 }
